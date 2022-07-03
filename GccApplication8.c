feb6e8815719c71d5850751a40ca8becafeee5aa
AnkitNikum <45768658+AnkitNikum@users.noreply.github.com>
2022-07-03 13:09:54 +0530

    Update GccApplication8.c

--- GccApplication8.c
+++ GccApplication8.c
@@ -18,3 +18,10 @@
 } } void adc_init(void) { //USART_Transmitchar('r',0); ADCSRA|=(1<<ADPS0)|(1<<ADPS1)|(1<<ADPS2);//|(1<<ADFR); ADMUX|=1<<REFS0;//SUPPLY 5V AT AVCC ADCSRA|=1<<ADEN; ADCSRA|=1<<ADSC; //USART_Transmitchar('t',0); }
 
 uint16_t adc_read(uint8_t channel) {
+ADMUX&=0xF0;
+ADMUX|=channel;																										;
+ADCSRA|=1<<ADSC;
+//USART_Transmitchar('g',0);
+while(ADCSRA&(1<<ADSC));
+//USART_Transmitchar('f',0);
+return ADCW;}
